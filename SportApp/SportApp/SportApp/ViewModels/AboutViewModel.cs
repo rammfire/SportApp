2c5bb279b68c1e9c5824ba410737796363367045
Денис <mail@denis-andreevich.ru>
2021-06-17 21:25:03 +0500

    Начало!

---
+++ SportApp/SportApp/SportApp/ViewModels/AboutViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace SportApp.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            ShowMessage = new Command(async () => await Browser.OpenAsync("https://tcmk74.ru"));
+        }
+
+
+
+        public ICommand ShowMessage { get; }
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ SportApp/SportApp/SportApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SportApp.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
