4a50511ce9a90c24d846752897d6b5d615e83563
Денис <denis-andreevich@outlook.com>
2021-07-22 11:29:50 +0500

    Обновил класс

--- SportApp/SportApp/SportApp/ViewModels/TrainingViewModel.cs
+++ SportApp/SportApp/SportApp/ViewModels/TrainingViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using SportApp.Models;
 using Xamarin.Forms;
+using System.Windows.Input;
 
 namespace SportApp.ViewModels
 {
@@ -13,7 +14,14 @@ namespace SportApp.ViewModels
         ObservableCollection<PowerTraining> powerTrainings;
 
         public ObservableCollection<PowerTraining> PowerTrainings { get { return powerTrainings; } set { powerTrainings = value; OnPropertyChanged(); } }
+        public ICommand TypeListSelectedCommand => new Command<PowerTraining>(selectedItem);
 
+        void selectedItem(PowerTraining obj)
+        {
+            PowerTraining item = obj as PowerTraining;
+
+            System.Diagnostics.Debug.WriteLine("clicked item = " + item.DayOfTraining);
+        }
         public TrainingViewModel()
         {
                 PowerTrainings = new ObservableCollection<PowerTraining>();
